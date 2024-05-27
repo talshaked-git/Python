438c30fb9391663b54b38eef8098f6dc8922ac2d
Tal Shaked <73061047+talshaked-git@users.noreply.github.com>
2024-05-27 13:47:54 +0300

    more slidingwindow problems

--- Techniques/SlidingWindow.py
+++ Techniques/SlidingWindow.py
@@ -1,8 +1,8 @@
 
 # Find the max sum subarray of a fixed size K
 # Example input:
-# [ 4, 2, 1 , 7 , 8, 1 , 2 ,8, 1 ,0]
-
+# [ 4, 2, 1 , 7 , 8, 1 , 2 ,8, 1 ,0], k = 3
+INT_MAX = 2**31 - 1
 INT_MIN = -2**31
 
 def MaxSumSubArray(arr, k):
@@ -19,6 +19,37 @@ def MaxSumSubArray(arr, k):
     
     return maxSum
 
-arr = [1, 3, 2, 6, -1, 4, 1, 8, 2]
-k = 3
+arr = [-1, 2, 3, 1, -3, 2]
+k = 2
 print(MaxSumSubArray(arr, k))
+
+
+# Find the smallest subarray with given sum
+# Example input:
+# [4, 2, 2, 7, 1, 1, 2, 2, 1, 0], sum = 8
+
+def smallestSubarray(arr, sum):
+    currentWindowSum = 0
+    windowStart = 0
+    minWindowSize = INT_MAX
+    for windowEnd in range(len(arr)):
+        currentWindowSum += arr[windowEnd]
+
+        while(currentWindowSum >= sum):
+            minWindowSize = min(minWindowSize,windowEnd - windowStart + 1)
+            currentWindowSum -= arr[windowStart]
+            windowStart += 1
+    
+    return minWindowSize
+
+
+
+arr = [4, 2, 2, 7, 8, 1, 2, 8, 1, 0]
+sum = 8
+print(smallestSubarray(arr, sum))
+
+
+
+# Find the longest substring length with k distinct characters
+# Example input:
+# s = "AAAHHIBC", k = 
