f5d1a18883b66eb109dc1bee0bbdbac23333a01a
Tal Shaked <73061047+talshaked-git@users.noreply.github.com>
2024-05-28 15:09:34 +0300

    worked on SQL and added int to roman

---
+++ LeetCode/integerToRoman.py
@@ -0,0 +1,17 @@
+
+
+def intToRoman(num):
+    romanNum = ''
+
+    romanSymbol = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+
+    for value, symbol in romanSymbol:
+        while num >=value:
+            romanNum += symbol
+            num -= value
+    
+    return romanNum
+
+
+num = 3749
+print(intToRoman(num))
